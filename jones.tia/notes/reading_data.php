8a3789cba1a173cedc01676ee2e78c23b4559e20
tjones43artu <tjones43@art.edu>
2025-05-12 18:09:33 -0400

    Update reading_data.php

--- jones.tia/notes/reading_data.php
+++ jones.tia/notes/reading_data.php
@@ -1,19 +1,7 @@
-<?php
+<?php 
 
 include "../lib/php/functions.php";
 
-$filename = "notes.json";
-$file = file_get_contents($filename);
-$notes = json_decode($file);
-
-$filename = "notes.json";
-$file = file_get_contents($filename);
-$notes = json_decode($file);
-
-$notes_object = file_get_json("notes.json");
-$users_array = file_get_json("../data/notes.json");
-
-// print_p($notes)
 
 ?>
 <!DOCTYPE html>
@@ -22,38 +10,26 @@ $users_array = file_get_json("../data/notes.json");
 	<meta charset="UTF-8">
 	<title>Reading Data</title>
 
-<?php include "parts/meta.php" ?>
+	<?php include "../parts/meta.php"; ?>
 </head>
 <body>
 
-	<?php include "parts/navbar.php" ?>
-		
+	<?php include "../parts/navbar.php"; ?>	
+	
 	<div class="container">
-		<div class="card soft">
-			<h2>Notes</h2>
-			
-			<?php
-			
-			for($i=0;$i<count($notes_object->notes;$1++){
-				echo "<li>{$notes_object->notes[$i]}</li>";
-			}
-			
-			?>	
+			<div class="card soft">
+				<h2>Notes</h2>
+				
+	<?php 
+	$filename = "notes.json";
+	$file = file_get_contents($filename);
+	$notes = json_decode($file);
+
+
+	print_p($notes); ?>
+
+			</div>
 		</div>
-		<div class="card soft">
-			<h2>Users</h2>
-			
-			<?php
-			
-			for($i=0;$i<count($users_array);$1++){
-				echo "<li>
-					<strong>{$notes_object->notes[$i]}</strong>
-					<span>{$users_array[$i]->type}</span>
-				</li>";
-			}
-			
-			?>	
-		</div>
-	</div>
+
 	</body>
-	</html>
+</html>
